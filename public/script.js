6a5e598c019764ddcfccd00a5d2157e7489ff767
EvaMary <evamary82@gmail.com>
2023-06-04 23:46:28 +1000

    getting the tagify function working

--- public/script.js
+++ public/script.js
@@ -43,13 +43,13 @@ const tagsInput = document.getElementById('tags');
 // Initialize Tagify on the input element
 const tagify = new Tagify(tagsInput);
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
   event.preventDefault();//block default submission behaviour 
   const tagsArray = tagify.value.map((tag) => tag.value);
   console.log(form.elements.podcastName.value)
   addPodcast(
     form.elements.podcastName.value,
-    form.elements.episodeTitle.value, 
+    form.elements.episodeTitle.value,
     form.elements.episodeNumber.value,
     form.elements.platform.value,
     form.elements.host.value,
@@ -57,25 +57,25 @@ form.addEventListener("submit", function(event){
     form.elements.genre.value,
     form.elements.rating.value
   );
-  form.reset(); 
+  form.reset();
 })
-var podcastList =[];
-function addPodcast(podcastName, episodeTitle, episodeNumber, platform, host, tags,selectedGenre, rating) {
-let podcast = {
-  podcastName, 
-  episodeTitle,
-  episodeNumber,
-  platform,
-  host,
-  tags,
-  selectedGenre,
-  rating,
-  id: Date.now(),
-  date: new Date().toISOString(),
-};
-podcastList.push(podcast);
-localStorage.setItem('podcasts', JSON.stringify(podcastList));
-updatePodcastList();
+var podcastList = [];
+function addPodcast(podcastName, episodeTitle, episodeNumber, platform, host, tags, selectedGenre, rating) {
+  let podcast = {
+    podcastName,
+    episodeTitle,
+    episodeNumber,
+    platform,
+    host,
+    tags,
+    selectedGenre,
+    rating,
+    id: Date.now(),
+    date: new Date().toISOString(),
+  };
+  podcastList.push(podcast);
+  localStorage.setItem('podcasts', JSON.stringify(podcastList));
+  updatePodcastList();
 }
 if (localStorage.getItem('podcasts')) {
   podcastList = JSON.parse(localStorage.getItem('podcasts'));
@@ -91,7 +91,7 @@ function updatePodcastList() {
     podcastList.forEach((podcast) => {
       let gridItem = document.createElement('div');
       gridItem.className = 'grid-item';
-     
+
 
       let image = document.createElement('img');
       image.src = getGenreImageSource(podcast.selectedGenre);
@@ -108,9 +108,9 @@ function updatePodcastList() {
 
       for (let i = 0; i < podcast.rating; i++) {
         let starIcon = document.createElement('span');
-  starIcon.innerHTML = '&#9733;';
-  starIcon.className = 'filled-star-icon';
-  starsContainer.appendChild(starIcon);
+        starIcon.innerHTML = '&#9733;';
+        starIcon.className = 'filled-star-icon';
+        starsContainer.appendChild(starIcon);
       }
 
       gridItem.appendChild(image);
@@ -121,6 +121,10 @@ function updatePodcastList() {
       list.appendChild(gridItem);
       gridItem.dataset.id = podcast.id;
       list.appendChild(gridItem);
+      const genreImage = document.getElementById('genre-image');
+      const genre = podcast.selectedGenre;
+      const imageSource = getGenreImageSource(genre);
+      genreImage.src = imageSource;
     });
   }
 }
@@ -151,36 +155,36 @@ function getGenreImageSource(genre) {
 
   }
 }
- /*console.log(podcastList); 
- const inputs = [];
-  form.querySelectorAll("input").forEach((input) => {
-    const { name, value } = input;
-    inputs.push({ name, value });
-  });
-  console.log(inputs)
-  form.reset();
+/*console.log(podcastList); 
+const inputs = [];
+ form.querySelectorAll("input").forEach((input) => {
+   const { name, value } = input;
+   inputs.push({ name, value });
+ });
+ console.log(inputs)
+ form.reset();
 ;
-  data.forEach((country) => {
-        console.log(country)
-        
-        // Container for card (Div, with a class=".")
-        
-        
-        // Title: Heading == Country Name
-        
-        
-        // Description: Paragrph == Country Information
-        
-        
-        // Image: Image == Country Flag
-        
-        
-        // Appending all content to the card
-        
-        
-        // Append the content to the page
-        
-    })
+ data.forEach((country) => {
+       console.log(country)
+       
+       // Container for card (Div, with a class=".")
+       
+       
+       // Title: Heading == Country Name
+       
+       
+       // Description: Paragrph == Country Information
+       
+       
+       // Image: Image == Country Flag
+       
+       
+       // Appending all content to the card
+       
+       
+       // Append the content to the page
+       
+   })
 })
 */
 
@@ -216,7 +220,7 @@ genreItems.forEach((item) => {
     image.classList.add('active');
   });
 });
-function getPodcastById(podcastId){
+function getPodcastById(podcastId) {
   return podcastList.find((podcast) => podcast.id === parseInt(podcastId));
 }
 const gridItems = document.querySelectorAll('.grid-item');
@@ -231,122 +235,122 @@ gridItems.forEach((item) => {
     const podcastId = parseInt(item.dataset.id);
     const podcast = getPodcastById(podcastId);
 
- 
-      if (podcast){
-        gridItem = item;
-        const genreImage = document.getElementById('genre-image');
+
+    if (podcast) {
+      gridItem = item;
+      const genreImage = document.getElementById('genre-image');
       const genre = podcast.selectedGenre;
       const imageSource = getGenreImageSource(genre);
       genreImage.src = imageSource;
 
       document.getElementById('more-info-date').textContent = podcast.date;
-    document.getElementById('more-info-podcastName').textContent = podcast.podcastName;
-    document.getElementById('more-info-episodeTitle').textContent = podcast.episodeTitle;
-    document.getElementById('more-info-episodeNumber').textContent = podcast.episodeNumber;
-    document.getElementById('more-info-host').textContent = podcast.host;
-    document.getElementById('more-info-platform').textContent = podcast.platform;
-    document.getElementById('more-info-genre').textContent = podcast.selectedGenre;
-    document.getElementById('more-info-tags').textContent = podcast.tag;
+      document.getElementById('more-info-podcastName').textContent = podcast.podcastName;
+      document.getElementById('more-info-episodeTitle').textContent = podcast.episodeTitle;
+      document.getElementById('more-info-episodeNumber').textContent = podcast.episodeNumber;
+      document.getElementById('more-info-host').textContent = podcast.host;
+      document.getElementById('more-info-platform').textContent = podcast.platform;
+      document.getElementById('more-info-genre').textContent = podcast.selectedGenre;
+      document.getElementById('more-info-tags').textContent = podcast.tag;
 
-    const starsContainer = document.getElementById('stars-container');
-    starsContainer.innerHTML = ''; // Clear any existing stars
-    const rating = podcast.rating;
-    for (let i = 0; i < rating; i++) {
-      let starIcon = document.createElement('span');
-      starIcon.innerHTML = '&#9733;';
-      starIcon.className = 'filled-star-icon';
-      starsContainer.appendChild(starIcon);
-  }
-}
-    });
+      const starsContainer = document.getElementById('stars-container');
+      starsContainer.innerHTML = ''; // Clear any existing stars
+      const rating = podcast.rating;
+      for (let i = 0; i < rating; i++) {
+        let starIcon = document.createElement('span');
+        starIcon.innerHTML = '&#9733;';
+        starIcon.className = 'filled-star-icon';
+        starsContainer.appendChild(starIcon);
+      }
+    }
   });
-  
-  /*function getPodcastById(podcastId) {
-    const foundPodcast = podcastList.find((podcast) => podcast.id === podcastId);
-    console.log('Found podcast:', foundPodcast);
-    return foundPodcast;
-  }*/
-  moreInfoCloseButton.addEventListener('click', () => {
-    moreInfoContainer.classList.remove('active');
-  });
-  const trashcanIcon = document.getElementById('trashcan-icon');
-  const confirmationModal = document.getElementById('confirmation-modal');
-  const confirmDeleteButton = document.getElementById('confirm-delete');
-  const refuseDeleteButton = document.getElementById('refuse-delete');
-  trashcanIcon.addEventListener('click', function(){
-    confirmationModal.style.display="block"
-  })
-  refuseDeleteButton.addEventListener('click', function(){
-    confirmationModal.style.display = 'none';
-  })
-  confirmDeleteButton.addEventListener('click', function() {
-    if (gridItem && gridItem.dataset && gridItem.dataset.id) {
-      const podcastId = parseInt(gridItem.dataset.id);
-      deletePodcast(podcastId);
-    confirmationModal.style.display = 'none';
-    moreInfoContainer.classList.remove('active');
-  gridItem.style.display = 'none';
-  gridItem = null;// Reset gridItem after deletin
+});
 
-  // Update the podcast list
-  updatePodcastList();
+/*function getPodcastById(podcastId) {
+  const foundPodcast = podcastList.find((podcast) => podcast.id === podcastId);
+  console.log('Found podcast:', foundPodcast);
+  return foundPodcast;
+}*/
+moreInfoCloseButton.addEventListener('click', () => {
+  moreInfoContainer.classList.remove('active');
+});
+const trashcanIcon = document.getElementById('trashcan-icon');
+const confirmationModal = document.getElementById('confirmation-modal');
+const confirmDeleteButton = document.getElementById('confirm-delete');
+const refuseDeleteButton = document.getElementById('refuse-delete');
+trashcanIcon.addEventListener('click', function () {
+  confirmationModal.style.display = "block"
+})
+refuseDeleteButton.addEventListener('click', function () {
+  confirmationModal.style.display = 'none';
+})
+confirmDeleteButton.addEventListener('click', function () {
+  if (gridItem && gridItem.dataset && gridItem.dataset.id) {
+    const podcastId = parseInt(gridItem.dataset.id);
+    deletePodcast(podcastId);
+    confirmationModal.style.display = 'none';
+    moreInfoContainer.classList.remove('active');
+    gridItem.style.display = 'none';
+    gridItem = null;// Reset gridItem after deletin
+
+    // Update the podcast list
+    updatePodcastList();
   }
 });
 
-  function deletePodcast(podcastId) {
-    // Get the existing podcast list from local storage
-    let podcastList = JSON.parse(localStorage.getItem('podcasts'));
-  
-    // Find the index of the podcast with the matching ID
-    const index = podcastList.findIndex(podcast => podcast.id === podcastId);
-  
-    // If the podcast is found, remove it from the list
-    if (index !== -1) {
-      podcastList.splice(index, 1);
-  
-      // Save the updated podcast list back to local storage
-      localStorage.setItem('podcasts', JSON.stringify(podcastList));
-    }
-  }
-  /*confirmDeleteButton.addEventListener('click', function(){
-    const activePodcast = document.querySelector('.grid-item.active');
-  if (activePodcast) {
-    const podcastId = activePodcast.dataset.id;
-    deletePodcast(podcastId);
-    confirmationModal.style.display = 'none';
-  }
-  })
+function deletePodcast(podcastId) {
+  // Get the existing podcast list from local storage
+  let podcastList = JSON.parse(localStorage.getItem('podcasts'));
 
-  function deletePodcast(podcastId){
-    let podcastList = getPodcastItemsFromStorage();
-    podcastList = podcastList.filter((podcast) => podcast.id !== podcastId);
-    savePodcastItemsToStorage(podcastList);
-    removePodcastItemFromGrid()
+  // Find the index of the podcast with the matching ID
+  const index = podcastList.findIndex(podcast => podcast.id === podcastId);
 
-    const podcastItems = getPodcastItemFromStorage();
-    const index = podcastItems.findIndex(item => item.id === podcastId);
-    if (index !== -1){
-      podcastItems.splice(index,1);
-      savePodcastItemsToStorage(podcastItems);
-      removePodcastItemsFromGrid(podcastId);
-    }
-    function removePodcastItemFromGrid(podcastId){
-      const gridItem = document.querySelector('.grid-tem[data-id"${podcastId}"]');
-      if (gridItem);
-      gridItem.remove();
-    }
-  }*/
-  function getPodcastItemsFromStorage(){
-    let podcastList = localStorage.getItem('podcast');
-    if (podcastList){
-      return JSON.parse(podcastList);
-    }
-    return [];
+  // If the podcast is found, remove it from the list
+  if (index !== -1) {
+    podcastList.splice(index, 1);
+
+    // Save the updated podcast list back to local storage
+    localStorage.setItem('podcasts', JSON.stringify(podcastList));
   }
-  function savePodcastItemsToStorage(podcastList){
-    localStorage.setItem('podcast', JSON.stringify(podcastList));
+}
+/*confirmDeleteButton.addEventListener('click', function(){
+  const activePodcast = document.querySelector('.grid-item.active');
+if (activePodcast) {
+  const podcastId = activePodcast.dataset.id;
+  deletePodcast(podcastId);
+  confirmationModal.style.display = 'none';
+}
+})
+
+function deletePodcast(podcastId){
+  let podcastList = getPodcastItemsFromStorage();
+  podcastList = podcastList.filter((podcast) => podcast.id !== podcastId);
+  savePodcastItemsToStorage(podcastList);
+  removePodcastItemFromGrid()
+
+  const podcastItems = getPodcastItemFromStorage();
+  const index = podcastItems.findIndex(item => item.id === podcastId);
+  if (index !== -1){
+    podcastItems.splice(index,1);
+    savePodcastItemsToStorage(podcastItems);
+    removePodcastItemsFromGrid(podcastId);
   }
-  moreInfoCloseButton.addEventListener('click', () => {
-    moreInfoContainer.classList.remove('active');
-    console.log('more-info-container class:', moreInfoContainer.classList);
-  });
+  function removePodcastItemFromGrid(podcastId){
+    const gridItem = document.querySelector('.grid-tem[data-id"${podcastId}"]');
+    if (gridItem);
+    gridItem.remove();
+  }
+}*/
+function getPodcastItemsFromStorage() {
+  let podcastList = localStorage.getItem('podcast');
+  if (podcastList) {
+    return JSON.parse(podcastList);
+  }
+  return [];
+}
+function savePodcastItemsToStorage(podcastList) {
+  localStorage.setItem('podcast', JSON.stringify(podcastList));
+}
+moreInfoCloseButton.addEventListener('click', () => {
+  moreInfoContainer.classList.remove('active');
+  console.log('more-info-container class:', moreInfoContainer.classList);
+});
